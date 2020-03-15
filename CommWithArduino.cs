a41d437fd746502fb66fc55ba17e4a0e2154812e
chi541323 <38553011+chi541323@users.noreply.github.com>
2020-03-15 18:57:51 +0800

    Add files via upload

---
+++ CommWithArduino.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO.Ports;
+
+public class CommWithArduino : MonoBehaviour
+{
+    // set the port varilb
+    public string portName;
+    public int baudRate = 115200;
+    SerialPort arduinoSerial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arduinoSerial = new SerialPort(portName, baudRate);
+        arduinoSerial.ReadTimeout = 10;
+        arduinoSerial.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Read the data from the Arduino
+    public void ReadFromArduino()
+    {
+        string str = null;
+        try
+        {
+            str = arduinoSerial.ReadLine();
+            string[] words = str.Split(',');
+        }
+
+        catch (TimeoutException e)
+        {
+        }
+    }
+
+    //Send the servo motor's data to the arduino
+    public void SendToArduino(char MotorNum, int degree)
+    {
+        // check the port is available
+        if (arduinoSerial.IsOpen)
+        {
+            arduinoSerial.WriteLine(MotorNum + "," + degree);
+            Debug.Log(MotorNum + " , " + degree); // print the info
+        }
+    }
+}
